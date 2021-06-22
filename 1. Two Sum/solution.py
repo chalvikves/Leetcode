68023cde47e3a83df2a52b7c6c8d30cfd9c4056c
chalvikves <vikves@student.chalmers.se>
2021-06-22 22:20:42 +0200

    Created def and comment

---
+++ 1. Two Sum/solution.py
@@ -0,0 +1,9 @@
+
+def twoSum(self, nums, target): 
+    """
+     @param nums The input that is an array of integers
+     @param target The the target sum, calculated from the input array
+     * The algorithm should be less than O(n^2) time complexity
+    """
+
+
